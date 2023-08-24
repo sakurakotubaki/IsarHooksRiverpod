05d3d917f2a724a855144ca6016c99845b2f5573
[Flutter_SakuSaku13:44] <[gitonly543@gmail.com]>
2023-08-24 22:45:10 +0900

    fit: abstract class

--- lib/utils/db_service.dart
+++ lib/utils/db_service.dart
@@ -1,6 +1,13 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:isar/isar.dart';
 import 'package:isar_hooks/domain/person.dart';
+
+// DbServiceBaseクラスを作成する
+abstract class DbServiceBase {
+  Future<void> addData(Person person);
+  Future<void> deleteData(Person person);
+}
+
 // isarパッケージをインポートする
 final dbServiceProvider = Provider<DbService>((ref) => DbService(ref.read(isarProvider)));
 
@@ -9,17 +16,19 @@ final isarProvider = Provider<Isar>((ref) {
   throw UnimplementedError();
 });
 
-class DbService {
+class DbService implements DbServiceBase {
   DbService(this.isar);
 
   final Isar isar;
   // isarにデータを追加する関数
+  @override
   Future<void> addData(Person person) async {
     await isar.writeTxn(() async {
       await isar.persons.put(person);
     });
   }
   // isarからデータを削除する関数
+  @override
   Future<void> deleteData(Person person) async {
     await isar.writeTxn(() async {
       await isar.persons.delete(person.id);
